d564014f144b0bf18b4a35d2ef99bf796e552fef
a.ansimov <@WSX1qaz>
2018-02-11 15:08:17 +0200

    zuul

--- apigw/src/main/java/com/ogasimov/labs/springcloud/microservices/apigw/ApiGwApp.java
+++ apigw/src/main/java/com/ogasimov/labs/springcloud/microservices/apigw/ApiGwApp.java
@@ -2,8 +2,10 @@ package com.ogasimov.labs.springcloud.microservices.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGwApp {
     public static void main(String[] args) {
         SpringApplication.run(ApiGwApp.class, args);
