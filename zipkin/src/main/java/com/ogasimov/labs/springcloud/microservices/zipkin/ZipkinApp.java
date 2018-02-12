f77c949230aa1c4c6973fa031a6d19a394e255f0
a.ansimov <@WSX1qaz>
2018-02-12 13:25:07 +0200

    conf

--- zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
+++ zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
@@ -2,8 +2,14 @@ package com.ogasimov.labs.springcloud.microservices.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApp {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApp.class, args);
