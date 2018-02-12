015fec91a62e271df393eadd98a827ee08cf14cf
a.ansimov <@WSX1qaz>
2018-02-12 20:42:24 +0200

    event

--- guest/src/main/java/com/ogasimov/labs/springcloud/microservices/guest/GuestController.java
+++ guest/src/main/java/com/ogasimov/labs/springcloud/microservices/guest/GuestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GuestController {
     @Autowired
     private DinnerService dinnerService;
