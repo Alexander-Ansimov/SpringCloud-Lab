aa7c616f680cd54011c52fa8713a706ce30a9223
a.ansimov <@WSX1qaz>
2018-02-12 14:04:08 +0200

    zipkin + sleuth

--- user/src/main/java/com/ogasimov/labs/springcloud/microservices/user/User.java
+++ user/src/main/java/com/ogasimov/labs/springcloud/microservices/user/User.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 
 
 @SpringBootApplication
+@EnableHystrix
 @EnableScheduling
 @EnableEurekaClient
 @EnableFeignClients
--- zuul/src/main/java/com/ogasimov/labs/springcloud/microservices/zuul/Zuul.java
+++ zuul/src/main/java/com/ogasimov/labs/springcloud/microservices/zuul/Zuul.java
@@ -2,9 +2,11 @@ package com.ogasimov.labs.springcloud.microservices.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableHystrix
 @EnableZuulProxy
 public class Zuul {
     public static void main(String[] args) {
