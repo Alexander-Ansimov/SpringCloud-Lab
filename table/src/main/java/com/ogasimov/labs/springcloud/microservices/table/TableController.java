f719d99220e094fd117dcd48628382b8a07306fe
a.ansimov <@WSX1qaz>
2018-02-19 13:39:02 +0200

    event

--- table/src/main/java/com/ogasimov/labs/springcloud/microservices/table/TableController.java
+++ table/src/main/java/com/ogasimov/labs/springcloud/microservices/table/TableController.java
@@ -4,16 +4,22 @@ import java.util.List;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TableController {
 
     @Autowired
     private TableService tableService;
+
+    @Value("${testValue}")
+    private String testValue;
 
     @HystrixCommand
     @GetMapping("/tables")
@@ -37,5 +43,10 @@ public class TableController {
     @PutMapping("/table/{id}/occupy")
     public void occupyTable(@PathVariable Integer id) {
         tableService.updateTable(id, false);
+    }
+
+    @GetMapping
+    public String getTestValue() {
+        return "testValue = " + testValue;
     }
 }
